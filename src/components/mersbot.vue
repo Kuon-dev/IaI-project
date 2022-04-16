52695ef750a5282305e4155886b9ca414d807581
Remicaster <83005177+Remicaster@users.noreply.github.com>
2022-04-16 14:56:49 +0800

    update bot color

--- src/components/mersbot.vue
+++ src/components/mersbot.vue
@@ -1,8 +1,8 @@
 <template>
 <div class="float-right fixed z-20 max-h-[50rem]">
     <div class="flex flex-row fixed top-[260px] right-0 mr-8 z-30" :class="{showButton: isBotHidden}">
-        <h2 class="bg-black w-[23rem] text-2xl text-white text-center py-1" :class="{hidden: isBotHidden}">MersBot</h2>
-        <button class="bg-sky-500 w-[5rem] text-2xl text-white text-center py-1" @click="isBotHidden = !isBotHidden">{{isBotHidden ? 'Show' : 'Hide'}}</button>
+        <h2 class="bg-gradient-to-r from-cyan-600 to-sky-500 w-[23rem] text-2xl text-white text-center py-1" :class="{hidden: isBotHidden}">MersBot</h2>
+        <button class="bg-gradient-to-r from-sky-500 to-indigo-500 w-[5rem] text-2xl text-white text-center py-1" @click="isBotHidden = !isBotHidden">{{isBotHidden ? 'Show' : 'Hide'}}</button>
     </div>
     <div id="webchat" role="main" class="drop-shadow-xl" :class="{hidden: isBotHidden}"></div>
 </div>
