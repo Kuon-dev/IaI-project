f905cecfd25e78380c844338efa6745c410e9cc6
Remicaster <83005177+Remicaster@users.noreply.github.com>
2022-04-18 16:42:34 +0800

    added responsive design

--- src/components/mersbot.vue
+++ src/components/mersbot.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="float-right fixed z-20 max-h-[50rem]">
-    <div class="flex flex-row fixed top-[260px] right-0 mr-8 z-30" :class="{showButton: isBotHidden}">
+    <div class="flex flex-row fixed top-[60px] right-0 mr-8 z-30 xl:top-[260px]" :class="{showButton: isBotHidden}">
         <h2 class="bg-gradient-to-r from-cyan-600 to-sky-500 w-[23rem] text-2xl text-white text-center py-1" :class="{hidden: isBotHidden}">MersBot</h2>
         <button class="bg-gradient-to-r from-sky-500 to-indigo-500 w-[5rem] text-2xl text-white text-center py-1" @click="isBotHidden = !isBotHidden">{{isBotHidden ? 'Show' : 'Hide'}}</button>
     </div>
@@ -12,7 +12,7 @@
 export default {
     data() {
         return {
-            isBotHidden: false,
+            isBotHidden: true,
         }
     },
     methods: {
@@ -24,12 +24,22 @@ export default {
 </script>
 
 <style>
+
 #webchat {
+    position: fixed;
+    height: calc(100% - 100px);
+    top: 100px;
+    width: 30rem;
+    overflow: hidden;
+}
+@media only screen and (min-width: 1280px) {
+    #webchat {
     position: fixed;
     height: calc(100% - 300px);
     top: 300px;
     width: 30rem;
     overflow: hidden;
+}
 }
 
 .showButton {
